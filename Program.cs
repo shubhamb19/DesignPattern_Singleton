75318f89547472f72cf4dc0567db68cda31d8084
shubhamb19 <shubhambhuse19@gmail.com>
2024-10-12 23:21:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Singleton s1 = Singleton.GetInstance();
+
+          Singleton s2 = Singleton.GetInstance();
+            if (s1 != s2) 
+            {
+                Console.WriteLine("Not shearing the same responce");           
+            }
+            else 
+            {
+                Console.WriteLine(" shearing the same responce");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    internal class Singleton
+    {
+        //1. Declear pravite static field
+        private static Singleton instance;
+
+        //2. Private Constructor
+        private Singleton() { }
+
+        //3. GetInstance method which will be access from outside scope
+        //will return instance always when invoked 
+        //else return already created
+        public static Singleton GetInstance()
+        { 
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+        //4. any business logic method
+
+        
+
+    }
+}
